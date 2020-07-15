598ec97c92c70e8e0f6b1075b8abeec73ab5ff90
MCCMike <mccmike@MCCMikede-MacBook-Air.local>
2020-07-15 15:22:44 +0800

    Testing

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+//mongodb+srv://1234:1234@cluster0.mqoxs.mongodb.net/cluster0?retryWrites=true&w=majority
+dbPassword = 'mongodb+srv://1234:1234@cluster0.mqoxs.mongodb.net/cluster0?retryWrites=true'
+
+module.exports = {
+    mongoURI: dbPassword
+};
