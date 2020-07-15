e6d27dba6d6f6ce8326d786d1186c8b9be9b0fb9
MCCMike <mccmike@MCCMikede-MacBook-Air.local>
2020-07-15 15:56:21 +0800

    Testing

--- app.js
+++ app.js
@@ -12,7 +12,8 @@
 //Update Heroku:
 //git push heroku master
 
-//http://localhost:5000
+//Website:
+//http://localhost:3000
 
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
@@ -73,6 +74,6 @@ app.use(function(req, res, next) {
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,3 @@
-//mongodb+srv://1234:1234@cluster0.mqoxs.mongodb.net/cluster0?retryWrites=true&w=majority
 dbPassword = 'mongodb+srv://1234:1234@cluster0.mqoxs.mongodb.net/cluster0?retryWrites=true'
 
 module.exports = {
